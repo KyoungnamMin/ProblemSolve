6206f8b5fc1e4b6fe0ecd396ed0ed922605d9b69
KyoungnamMin <alsrudska97@naver.com>
2021-07-09 09:11:07 +0900

    BOJ

---
+++ BaekJoon/BOJ_5582.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+s1 = input().rstrip()
+s2 = input().rstrip()
+
+# 행: s1, 열: s2
+dp = [[0] * len(s2) for _ in range(len(s1)+1)]
+answer = 0
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        if s1[i-1] == s2[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+            answer = max(dp[i][j], answer)
+
+print(answer)
