c5a78202e3b79013ac8810ca5ccadab81ab06ea9
KyoungnamMin <alsrudska97@naver.com>
2021-07-23 23:57:58 +0900

    BOJ

--- BaekJoon/BOJ_1956.py
+++ BaekJoon/BOJ_1956.py
@@ -11,14 +11,14 @@ for i in range(E):
     a, b, c = map(int, input().split())
     s[a - 1][b - 1] = c
 
-for k in range(v):
-    for i in range(v):
-        for j in range(v):
+for k in range(V):
+    for i in range(V):
+        for j in range(V):
             if s[i][j] > s[i][k] + s[k][j]:
                 s[i][j] = s[i][k] + s[k][j]
 
 result = INF
-for i in range(v):
+for i in range(V):
     result = min(result, s[i][i])
 
 if result == INF:
