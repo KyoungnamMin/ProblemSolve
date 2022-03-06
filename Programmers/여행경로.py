eb6ed3e09aacdc9e8de2dc9cc517fee811152ed5
KyoungnamMin <alsrudska97@naver.com>
2022-03-06 17:26:07 +0900

    programmers

---
+++ Programmers/여행경로.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+def solution(tickets):
+    path = defaultdict(list)
+    
+    for start, end in tickets:
+        path[start].append(end)
+    
+    for key in path.keys():
+        path[key].sort()
+    
+    answer = []
+    stack = ["ICN"]
+    while stack:
+        airport = stack[-1]
+        if path[airport] != []:
+            stack.append(path[airport].pop(0))
+        else:
+            answer.append(stack.pop())
+    
+    return answer[::-1]
