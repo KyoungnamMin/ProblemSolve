5e407151eefd1a66da376392be8ef4df41c827a9
KyoungnamMin <alsrudska97@naver.com>
2022-03-07 18:52:57 +0900

    programmers

--- Programmers/괄호변환.py
+++ Programmers/괄호변환.py
@@ -1,56 +1,54 @@
-# 올바른 괄호 문자열 체크 
-def check_proper(p):
-    check = 0
-    for ch in p:
-        if ch == '(':
-            check+=1
-        elif ch == ')':
-            check-=1
-        if check < 0:
-            return False
-    return True
+def is_right(p):
+    stack = []
 
-
-# 균형잡힌 괄호 문자열 u, v 로 분리
-def balanced(p):
-    check = 0
-    u = ''
-    v = ''
-    for ch in p:
-        u += ch
-        if ch == '(':
-            check += 1
-        elif ch == ')':
-            check -= 1
-        if check == 0:
-            break
-    if not len(u) == len(p):
-        v = p[len(u):]
-    return u, v
-
-
-def calc4(u, v):
-    tmp = '('
-    tmp += solution(v)
-    tmp += ')'
-    u = u[1:-1]
-    for ch in u:
-        if ch == '(':
-            tmp += ')'
+    for i in range(len(p)):
+        if len(stack) > 0 and p[i] == ")" and stack[-1] == "(":
+            stack.pop()
         else:
-            tmp += '('
-    return tmp
+            stack.append(p[i])
+
+    return True if len(stack) <= 0 else False
+
+
+def seperate(p):
+    left_count = 0
+    right_count = 0
+    for idx, i in enumerate(p):
+        if i == "(":
+            left_count += 1
+        elif i == ")":
+            right_count += 1
+
+        if left_count == right_count:
+            return p[: idx + 1], p[idx + 1 :]  # u,v
+
+
+def batch_program(p):
+    # 1번
+    if p == "":
+        return p
+    # 2번
+    u, v = seperate(p)
+    # 3번
+    if is_right(u):
+        return u + batch_program(v)
+    else:
+        answer = "("
+        answer += batch_program(v)
+        answer += ")"
+
+        for u_s in u[1:-1]:
+            if u_s == "(":
+                answer += ")"
+            else:
+                answer += "("
+        return answer
 
 
 def solution(p):
-    if not p or check_proper(p):
+    if is_right(p):
         return p
-    
-    u, v = balanced(p)
-    # u가 올바른 괄호 문자열
-    if check_proper(u):
-        return u + solution(v)
-    # u가 올바르지 않은 괄호 문자열
-    else:
-        return calc4(u, v)
-    
+
+    answer = batch_program(p)
+
+    return answer
--- Programmers/여행경로.py
+++ Programmers/여행경로.py
@@ -1,14 +1,15 @@
 from collections import defaultdict
 
+
 def solution(tickets):
     path = defaultdict(list)
-    
+
     for start, end in tickets:
         path[start].append(end)
-    
+
     for key in path.keys():
         path[key].sort()
-    
+
     answer = []
     stack = ["ICN"]
     while stack:
@@ -17,5 +18,5 @@ def solution(tickets):
             stack.append(path[airport].pop(0))
         else:
             answer.append(stack.pop())
-    
+
     return answer[::-1]
