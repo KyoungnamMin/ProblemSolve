cb350e562916ded1765a07f7c3ccb8539c4f0892
KyoungnamMin <alsrudska97@naver.com>
2022-04-12 18:20:57 +0900

    수 정렬 DP

---
+++ BaekJoon/길라잡이/BOJ_10989.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def solution():
+    input = sys.stdin.readline
+    N = int(input())
+    arr = [0] * 10001
+    for _ in range(N):
+        num = int(input())
+        arr[num] += 1
+
+    for i in range(1, 10001):
+        if arr[i] != 0:
+            for _ in range(arr[i]):
+                print(i)
+
+
+if __name__ == "__main__":
+    solution()
