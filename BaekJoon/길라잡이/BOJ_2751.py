10adb0c83acb1ee4b1ef19f07f35134e2e5c91a0
KyoungnamMin <alsrudska97@naver.com>
2022-04-12 18:20:26 +0900

    수 정렬

---
+++ BaekJoon/길라잡이/BOJ_2751.py
@@ -0,0 +1,30 @@
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+def solution1():
+    from queue import PriorityQueue
+
+    # 우선순위 큐 방법
+    N = int(input())
+    que = PriorityQueue()
+
+    for _ in range(N):
+        x = int(input())
+        que.put(x)
+    while not que.empty():
+        print(que.get())
+
+
+def solution2():
+    # 그냥 배열 정렬 방법
+    N = int(input())
+    array = []
+    for _ in range(N):
+        x = int(input())
+        array.append(x)
+    array.sort()
+    for value in array:
+        print(value)
+
+
+if __name__ == "__main__":
+    # solution1()
+    solution2()
