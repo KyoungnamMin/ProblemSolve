1f7f4931bf29f98d8f890845d39c871feb2ca355
KyoungnamMin <alsrudska97@naver.com>
2022-04-12 17:54:04 +0900

    1920 수 찾기 이분탐색

---
+++ BaekJoon/길라잡이/BOJ_1920.py
@@ -0,0 +1,35 @@
+global N_list
+
+
+def binary_search(N, key):
+    global N_list
+
+    start = 0
+    end = N - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if key < N_list[mid]:
+            end = mid - 1
+        elif key > N_list[mid]:
+            start = mid + 1
+        else:  # x==mid
+            return 1
+    return 0
+
+
+def solution():
+    global N_list
+    N = int(input())
+    N_list = list(map(int, input().split()))
+    N_list.sort()
+
+    M = int(input())
+    M_list = list(map(int, input().split()))
+
+    for x in M_list:
+        print(binary_search(N, x))
+
+
+solution()
